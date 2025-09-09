7f3b2f37d233f0159c65ada3b5639b69567afbdb
aaronbanerjee123 <aaronbaner@outlook.com>
2025-09-09 16:09:54 -0700

    initialized admin and clientside firebase implementations

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp, getApp, getApps} from "firebase/app"
+import {getAuth} from "firebase/auth";
+import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +15,9 @@ const firebaseConfig = {
   messagingSenderId: "1055648309283",
   appId: "1:1055648309283:web:bfa934d9daf8e56593d196",
   measurementId: "G-4BQNJTD82Y"
-};
+}; 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
