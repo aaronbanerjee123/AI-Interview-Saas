7e177d39a66769b16d419ffde1fd4cf419208dc8
aaronbanerjee123 <aaronbaner@outlook.com>
2025-09-08 21:31:30 -0700

    AuthForm updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -9,7 +10,7 @@ const monaSans = Mona_Sans({
 
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "An AI powered platform for preparing for mock interviews",
+  description: "An AI p owered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
@@ -20,9 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${monaSans.variable} antialiased`}
+        className={`${monaSans.variable} antialiased pattern`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
